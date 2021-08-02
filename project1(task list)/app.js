37e3e7da974dbac27ce6db6872459617fcf5a4bf
Gupta-Sanskriti <88326863+Gupta-Sanskriti@users.noreply.github.com>
2021-08-02 22:20:32 +0530

    Add files via upload

---
+++ project1(task list)/app.js
@@ -0,0 +1,7 @@
+// define UI vars
+
+const form = document.querySelector('#task-form');
+const taskInput = document.querySelector("#task-area");
+const clearBtn = document.querySelector('#clear-btn');
+
+// load all event listners function calls
